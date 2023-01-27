bd7b6b06a5c4cb858d0487d5bd16ce54dcfe4212
sarahtech2022 <106985498+sarahtech2022@users.noreply.github.com>
2023-01-26 21:39:55 -0700

    Added more changes to page

--- Recipe/script.js
+++ Recipe/script.js
@@ -1,7 +1,5 @@
 const otherRecipes = ["https://lubnabenhalim.ly/magrood/", "https://thebitterolive.com/libyan-soup/", "https://deliciouslymediterranean.com/authentic-shakshuka/"];
 
-
-
 function showList(array){
   const myDiv = document.getElementById("recipelinks");
   const myList = document.createElement("ol"); //creating the ol element
@@ -9,15 +7,16 @@ function showList(array){
   for(let i = 0; i < array.length; i++){
     let link = array[i];
     console.log(link);
-    let newsLi = document.createElement("li"); // same as line 7, here we are creating an li element
-    newsLi.innerHTML = `<a href=${link}> ${link} </a>`; //the li element i created on 12, can have innerText, or innerHTML -> they specify what goes inside li element (the actual text), innertext-> it prints out the literal angle brackets < >. innerHTML -> will treat those brackets as actual HTML.
+    let recipeList = document.createElement("li"); // same as line 7, here we are creating an li element
+    recipeList.innerHTML = `<a href=${link}> ${link} </a>`; //the li element i created on 12, can have innerText, or innerHTML -> they specify what goes inside li element (the actual text), innertext-> it prints out the literal angle brackets < >. innerHTML -> will treat those brackets as actual HTML.
     // a is anchor its putting a link on the page, href-> is the refrence for the link.
+
     // So esentially wht I am doing on line 13 is using innerHTML to add my link from the text in array[i]
     // first link is inside the href (where u want the link to go) and the second is in the anchor (what is clickable).
-    myList.appendChild(newsLi);
+    myList.appendChild(recipeList);
+    
   }
 }
-
 showList(otherRecipes);
 // You dont always need to return a function!
 
