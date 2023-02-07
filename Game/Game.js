42da6042fde328ec2b69403f4a320348034877ac
sarahtech2022 <106985498+sarahtech2022@users.noreply.github.com>
2023-02-06 21:57:44 -0700

    Added radio buttons and submit button

--- Game/Game.js
+++ Game/Game.js
@@ -1,2 +1,12 @@
 let flipButton = document.querySelector(“#flipbutton”);
 let resetButton = document.querySelector(“#resetbutton”);
+
+let heads = 0;
+let tails = 0;
+
+flipButton.addEventListener(“click”, () => {
+    let i = Math.floor(Math.random() * 2);
+
+    if(i){heads++;}else{
+        tails++;
+    }
