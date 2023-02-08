91aafff99ebb40b7aaa9cbe7c5d5b973fd43cb9d
sarahtech2022 <106985498+sarahtech2022@users.noreply.github.com>
2023-02-07 22:33:13 -0700

    adding js functionality to the game and added a result div

--- Game/Game.js
+++ Game/Game.js
@@ -3,7 +3,7 @@ let resetButton = document.querySelector(“#resetbutton”);
 
 let heads = 0;
 let tails = 0;
-// This is saying immediatey disable the button, then 3 seconds later reenable it, (milliseconds)
+// This is saying immediatey disable the button, then 5 seconds later re-enable it, (milliseconds)
 function disableButton(){
     flipButton.disabled = true;
     setTimeout(function(){
@@ -21,3 +21,32 @@ flipButton.addEventListener("click", () => {
     }
     disableButton();
 });
+
+//1. Check which radio button is selected
+
+const headSelected= document.querySelector('#heads');
+const tailsSelected= document.querySelector('#tails');
+const radioButtons = document.querySelectorAll('input[name="radio"]');
+
+//2. Second, register a click event listener on the button element:
+flipbutton.addEventListener('click', () => {
+});
+
+//3. Third, iterate over the radio buttons and get the value of the selected radio button:
+let selectedSize;
+for (const radioButton of radioButtons) {
+  if (radioButton.checked) {
+    selectedSize = radioButton.value;
+    break;
+  }
+}
+
+//4. Finally set the message for the output element:
+output.innerText = selectedSize ? `You selected ${selectedSize}` : `You haven't selected any size`;
+
+
+// if(document.getElementById('heads').checked == true) {   
+//     document.write("Heads radio button is selected");   
+// } else {  
+//     document.write("Tails radio button is selected");   
+// }  
