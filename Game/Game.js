304c6fe0f003dbbca1a6f63822333b279cc87cfd
sarahtech2022 <106985498+sarahtech2022@users.noreply.github.com>
2023-02-07 19:06:03 -0700

    removed submit button because not needed and added disable butt on function so that the flip button is disabled for 5 sec after clicking

--- Game/Game.js
+++ Game/Game.js
@@ -3,10 +3,21 @@ let resetButton = document.querySelector(“#resetbutton”);
 
 let heads = 0;
 let tails = 0;
+// This is saying immediatey disable the button, then 3 seconds later reenable it, (milliseconds)
+function disableButton(){
+    flipButton.disabled = true;
+    setTimeout(function(){
+        flipButton.disabled = false;
+    },5000);
+}
 
-flipButton.addEventListener(“click”, () => {
+flipButton.addEventListener("click", () => {
     let i = Math.floor(Math.random() * 2);
+    // number would be 0 or 1
 
-    if(i){heads++;}else{
+   // 0 or 1- any non zero number is true. 1=true, 0=false
+    if(i === 1){heads++;}else{
         tails++;
     }
+    disableButton();
+});
