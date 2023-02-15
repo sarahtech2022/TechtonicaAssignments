0636f73115bc94f97858612bef54599b852b750d
sarahtech2022 <106985498+sarahtech2022@users.noreply.github.com>
2023-02-14 19:04:33 -0700

    trying to figure out how to get cards to have random placement

--- Game2/src/App.js
+++ Game2/src/App.js
@@ -8,6 +8,17 @@ const arrayOfPics = ["https://static.thenounproject.com/png/1142266-200.png",
 "https://static.thenounproject.com/png/1242759-200.png",
 "https://static.thenounproject.com/png/1340537-200.png"];
 
+//Gives me a number 0 to 3. 
+//Give each card a random number, and arrange the cards by
+// 0-7 placement, so each time the card goes in a new place.
+// Return an array with 8 elements, with values 0-3, each number should 
+//be repeated twice and in a random order
+
+onclick = function(){return randomArray Math.floor(Math.random() * 4 );
+};
+
+{/* <button onclick="myFunction()">Click me</button> */}
+
 function App() {
   return (
     <div id="mainAppDiv" className="App">
