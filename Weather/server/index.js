ff329609b6af5d8e99fc8a2fdf775215c20cdd50
sarahtech2022 <106985498+sarahtech2022@users.noreply.github.com>
2023-02-27 13:44:49 -0700

    set up our node and express

---
+++ Weather/server/index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+const PORT = 5000;
+
+app.listen(PORT, () => console.log(`Hola! Server is running on PORT ${PORT}`));
