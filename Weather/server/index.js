efbd24e09ddb135d0f8a8ce2f9adfe4f4bfef139
sarahtech2022 <106985498+sarahtech2022@users.noreply.github.com>
2023-02-27 18:36:58 -0700

    fixed my server folder using Cristinas template

--- Weather/server/index.js
+++ Weather/server/index.js
@@ -1,7 +1,23 @@
 const express = require("express");
 const cors = require("cors");
-const app = express();
-app.use(cors());
-const PORT = 5000;
+require("dotenv").config();
+const dataWeather = require("./data");
 
+const app = express();
+
+const PORT = 8080;
+app.use(cors());
+app.use(express.json());
+
+// creates an endpoint for the route /api
+app.get("/", (req, res) => {
+  res.json({ message: "Hello from My template ExpressJS" });
+});
+
+// creates an endpoint for the route /api/weather
+app.get("/api/weather", (req, res) => {
+  res.json(dataWeather);
+});
+
+// console.log that your server is up and running
 app.listen(PORT, () => console.log(`Hola! Server is running on PORT ${PORT}`));
