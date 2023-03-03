3c60dc5c0caaa75765c64a8bd0228f320b39f8c0
sarahtech2022 <106985498+sarahtech2022@users.noreply.github.com>
2023-03-03 14:03:38 -0700

    reset up my project

--- Weather/client/src/App.js
+++ Weather/client/src/App.js
@@ -7,8 +7,29 @@ function App() {
   const [city, setCity] = useState("");
   const [result, setResult] = useState(null);
 
-  //A function to do the get request and set the state from openweather api
-  const loadCity = (city) => {
-    // pass city name as a param 
-    const params = new URLSearchParams({ cityName: city });
-    // fetch the data from the backe
+  //A function to do the get request and set the state from the hard code data
+  const loadCity = () => {
+    fetch("http://localhost:8080/api/weather")
+      .then((response) => response.json())
+      .then((result) => {
+        //console.log(result)
+        setCity(result.weather[0].name);
+        setResult(result);
+      });
+  }
+
+ const handleSubmit = (e) =>{
+  e.preventDefault();
+  loadCity();
+ }
+
+
+  return (
+    <div className="App">
+      <WeatherForm city={city} handleSubmit={handleSubmit}/>
+      {!result ? <p>Please click the botton to see Data</p> : <WeatherCard data={result} /> }
+    </div>
+  );
+}
+
+export default App;
--- Weather/client/src/components/weatherForm.js
+++ Weather/client/src/components/weatherForm.js
@@ -1,20 +1,22 @@
-const WeatherForm = (props) => {
-  return (
-    <div className="weather">
-      <h1 className="App-header">Techtonica Weather Forecast App</h1>
-      <form onSubmit={props.handleSubmit}>
-        <input
-          id="city-name"
-          type="text"
-          placeholder="Please enter the city name"
-          name="city"
-          value={props.city}
-          onChange={props.handleChangeCity}
-        />
-        <input type="submit" value="Submit" />
-      </form>
-    </div>
-  );
-};
+const WeatherForm = (props) =>{
+
+
+    return (
+        <div className="weather">
+        <h1 className="App-header">Techtonica Weather Forecast App</h1>
+        <form onSubmit={props.handleSubmit}>
+          <input
+            id="city-name"
+            type="text"
+            placeholder="Please enter the city name"
+            name="city"
+            value={props.city}
+            readOnly
+          />
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    )
+}
 
 export default WeatherForm;
