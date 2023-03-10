4863ba298f3f0c03caf2a42184ea06166979e2a8
sarahtech2022 <106985498+sarahtech2022@users.noreply.github.com>
2023-03-09 19:42:43 -0700

    added disable feature for buttons

--- week8Game/client/src/components/questioncard.js
+++ week8Game/client/src/components/questioncard.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import "./question.css";
 const QuestionCard = (props) => {
   const [answer, setAnswer] = useState("");
-
+  const [disable, setDisable] = useState(false);
   //dont need to declare the parameter userAnswer
   //user answer will have a value of true or false depending on lines 15 and 16, and thats because of the onclick!
   //the onclick- when the true button is clicked, we call the function compare answers, if the user clicks true the value of userAnswer is true
@@ -12,9 +12,11 @@ const QuestionCard = (props) => {
       // to update you call it, with the new value inside
       //This means answer now equals true
       props.counterFunction(true);
+      setDisable(true);
     } else {
       setAnswer(false);
       props.counterFunction(false);
+      setDisable(true);
     }
   };
   //callback function to game after, to tell game component whether user got it right or wrong.
@@ -33,8 +35,12 @@ const QuestionCard = (props) => {
       {/* the first question is referencing the prop passed in from the game component,
       the second question is the question key from the actual array of objects of questions! */}
       <div className="answer-section">
-        <button onClick={() => compareAnswers("True")}>True</button>
-        <button onClick={() => compareAnswers("False")}>False</button>
+        <button disabled={disable} onClick={() => compareAnswers("True")}>
+          True
+        </button>
+        <button disabled={disable} onClick={() => compareAnswers("False")}>
+          False
+        </button>
         {answer === "" ? null : (
           <p>That was {answer === true ? "true" : "false"}!</p>
         )}
