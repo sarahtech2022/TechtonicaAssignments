3fb6c966737c29c2e020efa5f3e8b1cfb9552713
sarahtech2022 <106985498+sarahtech2022@users.noreply.github.com>
2023-03-09 21:20:17 -0700

    changed title

--- week8Game/client/src/components/header.js
+++ week8Game/client/src/components/header.js
@@ -1,12 +1,11 @@
 const Header = (props) => {
-
   let message;
   if (props.user) {
-    message = `Welcome to my game ${props.user}`;
+    message = `Welcome to the Science Trivia Game, ${props.user}!`;
   } else {
-    message = `Welcome to my game`;
+    message = `Welcome to the Science Trivia Game`;
   }
-  
+
   return (
     <div className={"textHeader"}>
       <h3>{message}</h3>
